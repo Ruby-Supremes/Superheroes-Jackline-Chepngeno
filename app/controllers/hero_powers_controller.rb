b70b5b998d0fcb046798b981d8b89b6e1a4d30b9
jackline chepngeno <chepngenojackline0@gmail.com>
2022-10-09 21:15:49 +0300

    add create route to hero_powers

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,18 @@
 class HeroPowersController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :validation_error
+    def create
+        hero = HeroPower.create!(heropower_param)
+        render json: hero,  status: 201
+      end
+    
+      private 
+    
+      def validation_error
+        render json: { error:  ["validation errors"]}, status: 422
+      end
+    
+      def heropower_param
+        params.permit(:strength, :power_id, :hero_id)
+      end
+      
 end
