8c50138edc74dec9f77afdab2681db28350d9afd
jackline chepngeno <chepngenojackline0@gmail.com>
2022-10-09 20:56:02 +0300

    add routes

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,2 +1,23 @@
 class HeroesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        heroes = Hero.all
+        render json: heroes, include: ['id','name','super_name']
+    end
+
+    def show
+        hero = find_hero
+        render json: hero
+    end
+
+    private
+    
+    def render_not_found_response
+        render json: { error: "Hero not found" }, status: 404
+    end
+
+    def find_hero
+        Hero.find(params[:id])
+    end
 end
