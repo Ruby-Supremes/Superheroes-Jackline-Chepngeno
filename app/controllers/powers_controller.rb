5f03e14a429276108d6bb0c822110faedd3c9a55
jackline chepngeno <chepngenojackline0@gmail.com>
2022-10-09 21:05:40 +0300

    add routes to power_controller

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,38 @@
 class PowersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+rescue_from ActiveRecord::RecordInvalid, with: :validation_error
+
+    def index
+        powers = Power.all
+        render json:  powers, include: ['id','name','description']
+    end
+
+    def show
+        power = find_power
+        render json: power
+    end
+
+    def update
+        power = find_power
+        power.update(power_params)
+        render json: power
+    end
+
+    private
+    def power_params
+        params.permit(:name, :description)
+    end
+    
+    def find_power
+        Power.find(params[:id])
+    end
+    
+    def render_not_found_response
+        render json: { error: "Power not found" }, status: 404
+    end
+
+    def validation_error
+        render json: { error:  ["validation errors"]}, status: 422
+    end
+    
 end
